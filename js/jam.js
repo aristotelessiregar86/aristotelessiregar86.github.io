220acc2d07d08afc11deb52c09e6a6aca3790b4a
aristotelessiregar86 <aristotelessiregar89@gmail.com>
2021-03-09 12:01:08 +0700

    initial commit

--- js/jam.js
+++ js/jam.js
@@ -6,8 +6,39 @@ $(document).ready(function(){
             var hr = now.getHours();
             var Time = hr + ":" + mins + ":" + secs;
             document.getElementById("watch").innerHTML = Time;
+            
+            var tanggallengkap = new String();
+            var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+            namahari = namahari.split(" ");
+            var namabulan = ("Januari Februari Maret April Mei Juni Juli Agustus September Oktober November Desember");
+            namabulan = namabulan.split(" ");
+            var tgl = new Date();
+            var hari = tgl.getDay();
+            var tanggal = tgl.getDate();
+            var bulan = tgl.getMonth();
+            var tahun = tgl.getFullYear();
+            tanggallengkap = namahari[hari] + ", " +tanggal + " " + namabulan[bulan] + " " + tahun;
+            document.querySelector('#time').innerHTML = tanggallengkap;
+
+            // Peringatan Tahun Baru Dan Akan Di Aktifkan Pada Saat Malam Tahun Baru :) Have A Nice Day
+            if (tanggal == 10) {
+                //     document.querySelector('#selamat').innerHTML = `<div id="selamat1"><br><br><br><br><br><br><br><br><br>
+                //     <h5 class="text-center">Selamat Tahun Baru</h5>
+                //     <img class="gif" src="img/NewYear.gif" alt="">
+                // </div>`
+                
+                document.querySelector('#selamat').innerHTML = `<div id="selamat1"><br><br><br><br><br><br><br><br><br>
+                    <h5 class="text-center">Selamat Hari Persatuan Artis Film Indonesia(PARFI)</h5>
+                    <img class="gif" src="img/SelamatPAFRI.gif" alt="">
+                    </div>`
+            }
+
             requestAnimationFrame(clock);
         }
 
         requestAnimationFrame(clock);
     });
+
+ 
+
+
